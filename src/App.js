e56bab7d5d3c2f96ae3c0a98d4699b54cd872dac
michaelmocioiu <michaelmocioiu@gmail.com>
2024-11-03 14:05:33 -0500

    labweek9

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        studentID: '101459108',
+        name:'Michael Mocioiu',
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>
+              Welcome to Fullstack Development - I
+          </h1>
+          <h2>
+            React JS Programming Week09 Lab Exercise
+          </h2>
+          <h3>
+            {this.state.studentID}
+          </h3>
+          <h4>
+            {this.state.name}
+          </h4>
+          <h5>
+            George Brown College, Toronto
+          </h5>
+      </div>
+    )
+  }
 }
 
 export default App;
